9c9f9350b35196ad8e1479ef3a9cebe353647cf3
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-06-19 23:02:56 -0400

    [JESUS] - Update functions lose_message and start_game_two_or_more_players

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import random
 
-def lose_message():
-     print ("YOU LOSE !")
+def lose_message(name_player):
+     print (f"YOU LOSE {name_player.upper()}!")
      print("Better luck next time !")
      exit(0)
 
@@ -19,16 +19,32 @@ def insert_number(amount_of_numbers_to_enter,numbers_entered):
         number = int(input(">"))
         numbers_entered.append(number)
 
-def start_game_two_or_more_players(first_player, second_player,numbers_entered):
+def start_game_two_or_more_players(first_player, second_player,numbers_list):
+    last_number = 0
+    checked_list = False
+    turn = 1
     print(f"{first_player} is the first to play")
-    while numbers_entered[-1] != 21:
+    while True:
+        if turn == 1:
+            if checked_list == True:
+                lose_message(first_player)
+            elif last_number == 20:
+                lose_message(first_player)
+            print(f"Turn of {first_player}")
+            turn = 2
+        else:
+            if checked_list == True:
+                lose_message(second_player)
+            elif last_number == 20:
+                lose_message(second_player)
+            print(f"Turn of {second_player}")
+            turn = 1
         amount_of_numbers_to_enter = int(input("How much numbers do you want to enter?>"))
-        insert_number(amount_of_numbers_to_enter,numbers_entered)
-        checked_list = check_order(numbers_entered)
-        if checked_list == True:
-            lose_message()
+        insert_number(amount_of_numbers_to_enter,numbers_list)
+        checked_list = check_order(numbers_list)
+        last_number = numbers_list[-1]
         print(numbers_entered)
-        print(f"Turn of {second_player}")
+        
 
 if __name__ == "__main__":
     answer = False
@@ -51,7 +67,7 @@ if __name__ == "__main__":
         print("Two or more playes")
         name_player_1 = input("Please player one enter your name: ")
         name_player_2 = input("Please player two enter your name: ")
-        turns = 1# random.randrange(1,number_of_players+1)
+        turns = random.randrange(1,number_of_players+1)
         if turns == 1:
             start_game_two_or_more_players(name_player_1,name_player_2,numbers_entered)
                                  
